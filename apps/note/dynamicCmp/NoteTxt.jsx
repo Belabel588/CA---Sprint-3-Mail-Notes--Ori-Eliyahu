f630f11c1c952a74be3e87ffc54a0645664ec591
Belabel588 <orih1402@gmail.com>
2024-05-25 18:47:15 +0300

    merge f-compose into main

--- apps/note/dynamicCmp/NoteTxt.jsx
+++
@@ -1,9 +0,0 @@
-export function NoteTxt(props) {
-    const { note } = props
-    // console.log(note.info.txt);
-    return <div>
-        <h1>ITS A TXT</h1>
-        <section className="note-txt-title" >title is :{note.info.title}</section>
-        <section className="note-txt" > txt is :{note.info.txt}</section>
-    </div>
-}
--- apps/note/dynamicCmp/NoteVideo.jsx
+++
@@ -1,5 +0,0 @@
-export function NoteVideo(props) {
-
-    // console.log(props);
-    return <h1>ITS A VIDEO</h1>
-}
