26e3a7ff0e6bc2db26d0eb1cd561906e7ec58fdc
Belabel588 <orih1402@gmail.com>
2024-05-25 18:43:54 +0300

    finish compose feature and it's functionality

--- apps/mail/cmps/MailCompose.jsx
+++ apps/mail/cmps/MailCompose.jsx
@@ -1,20 +1,41 @@
+const { useNavigate } = ReactRouterDOM
+
 
 export function MailCompose() {
+
+  const navigate = useNavigate()
+
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    navigate('/mail')
+  }
+
+  function quitCompose() {
+    navigate('/mail')
+  }
+
   return (
     <div className="mail-compose-container">
+      <form onSubmit={handleSubmit}>
 
-      <h3 className="new-mail-title">New Message </h3>
+        <div className="new-mail-title-container">
+          <h3 className="new-mail-title">New Message </h3>
+          <p className="close-compose-btn" onClick={quitCompose}>X</p>
+        </div>
 
-      <div className="mail-to-container">
-        <label htmlFor="mail-to-input" type="email">To</label>
-        <input id="mail-to-input" type="text" />
-      </div>
+        <div className="mail-to-container">
+          <label htmlFor="mail-to-input" type="email">To</label>
+          <input id="mail-to-input" type="text" />
+        </div>
 
-      <div className="mail-subject-container">
-        <label htmlFor="mail-subject-input">Subject</label>
-        <input id="mail-subject-input" type="text" />
-      </div>
-      <textarea id="mail-text-input" cols="80" rows="20"></textarea>
+        <div className="mail-subject-container">
+          <label htmlFor="mail-subject-input">Subject</label>
+          <input id="mail-subject-input" type="text" />
+        </div>
+        <textarea id="mail-text-input" cols="80" rows="20"></textarea>
+        <button>Send</button>
+      </form>
     </div >
 
   )
