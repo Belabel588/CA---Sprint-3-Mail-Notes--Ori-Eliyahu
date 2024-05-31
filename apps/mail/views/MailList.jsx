d4ae529389d8476c61354b394ac1070e0c5fbc1b
Belabel588 <orih1402@gmail.com>
2024-05-31 20:23:23 +0300

    fix search bar not finding with uppercase (not converting properly)

--- apps/mail/views/MailList.jsx
+++ apps/mail/views/MailList.jsx
@@ -9,9 +9,9 @@ export function MailList({ mails, search, onMarkAsRead, onMarkAsUnread, onSendTo
         {/* <h1 className="mail-list">Mails list</h1> */}
         {mails.filter((mail) => mail.status === 'inbox')
             .filter((mail) => {
-                return search.toLowerCase() === '' ? mail : mail.subject.toLowerCase().includes(search) ||
-                    mail.body.toLowerCase().includes(search) ||
-                    mail.from.toLowerCase().includes(search)
+                return search.toLowerCase() === '' ? mail : mail.subject.toLowerCase().includes(search.toLowerCase()) ||
+                    mail.body.toLowerCase().includes(search.toLowerCase()) ||
+                    mail.from.toLowerCase().includes(search.toLowerCase())
             }).map(mail => <div key={mail.id} className="mail-preview">
                 <MailPreview mail={mail} onMarkAsRead={onMarkAsRead} onMarkAsUnread={onMarkAsUnread} onSendToBin={onSendToBin} onDeleteMail={onDeleteMail} />
             </div>)}
