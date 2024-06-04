5053022f0377ee8ea2891bc61615fda9f1866adb
Belabel588 <orih1402@gmail.com>
2024-06-04 15:14:58 +0300

    improve about page

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,42 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+
+        <div className="about-body">
+
+            <div className="about-header">
+                <h1 className="about-title">Developers</h1>
+            </div>
+
+            <div className="devs">
+
+                <div className="dev1">
+
+                    <div>
+                        <img className="dev1-img" src="assets/mail-imgs/developer1.png" alt="" />
+                    </div>
+
+                    <div>
+                        <h1 className="dev-name">Ori Halfon</h1>
+                    </div>
+
+                    <div className="devs-icons">
+                        <div>
+                            <img className="linkedin-icon" src="assets/mail-imgs/linkedin.png" alt="" />
+                        </div>
+
+
+                        <div>
+                            <img className="gitHub-icon" src="assets/mail-imgs/gitHub.jpg" alt="" />
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+        </div>
     </section>
 }
